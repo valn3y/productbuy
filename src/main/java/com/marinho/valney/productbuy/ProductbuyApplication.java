01f827dc752995a57105d65aa9d1fc5a572633ad
valn3y <neymarinho.junior@gmail.com>
2021-10-20 18:16:51 -0400

    project started with springboot

---
+++ src/main/java/com/marinho/valney/productbuy/ProductbuyApplication.java
@@ -0,0 +1,13 @@
+package com.marinho.valney.productbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductbuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marinho/valney/productbuy/ProductbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinho.valney.productbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
